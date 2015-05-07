d1202df32a930af1cc04f1f6f2b91ca6f044f831
frame11 <frame11@redchan.it>
2015-05-06 21:28:37 -0400

    Split setUp into setUpClass() and setUp()
    Also added test_get_subverse_subscriber_count()

--- tests.py
+++ tests.py
@@ -3,16 +3,22 @@ import VAPy
 
 class VAPyTests(unittest.TestCase):
     
-    def setUp(self):
+    def setUpClass(self):
         self.key = input("Please enter api_key: ")
         self.uname = input("Please enter username: ")
         self.pwd = getpass.getpass("Please enter password: ")
+        
+        
+    def setUp(self):
         self.vapy = VAPy.VAPy(self.key, self.uname, self.pwd)
 
 
     def test_get_token(self):
         self.assertIn('Authorization', self.vapy.headers)
 
+
+    def test_get_subverse_subscriber_count(self):
+        self.assertIsInstance(self.vapy.get_subverse_subscriber_count("api"), int)
 
 if __name__ == '__main__':
     unittest.main()
