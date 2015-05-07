6dc9b873ef4dbab878d195e464be7808eec70701
frame11 <frame11@redchan.it>
2015-05-06 21:08:01 -0400

    Added subverse informational functions
    subverse_info() is actual api call, other functions parse resultant dict

--- VAPy.py
+++ VAPy.py
@@ -6,7 +6,7 @@ except:
         import json
 
 
-
+API_URL = 'https://fakevout.azurewebsites.net/api/v1/'
 TOKEN_URL = 'https://fakevout.azurewebsites.net/api/token'
 
 
@@ -27,3 +27,24 @@ class VAPy:
             return resp['access_token']
         else:
             return False
+
+
+
+    # SUBVERSE FUNCTIONS
+
+    def subverse_info(self, subverse):
+        url = API_URL + "v/{}/info".format(subverse)
+        r = requests.get(url, headers=self.headers) 
+        return json.loads(r.content)
+
+    def get_subverse_creation_date(self, subverse):
+        return self.subverse_info(subverse)["creationDate"]
+
+    def get_subverse_subscriber_count(self, subverse):
+        return int(self.subverse_info(subverse)["subscriberCount"])
+
+    def get_subverse_rated_adult(self, subverse):
+        return True if self.subverse_info(subverse)["ratedAdult"] == "true" else False
+
+    def get_subverse_sidebar(self, subverse):
+        return self.subverse_info(subverse)["sidebar"]
