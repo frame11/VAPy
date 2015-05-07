81ccf5904bd4cdddcf7ca1eeac85f6a424312322
frame11 <frame11@redchan.it>
2015-05-06 20:37:04 -0400

    Added minimal error checking to token authorization

--- VAPy.py
+++ VAPy.py
@@ -16,10 +16,14 @@ class VAPy:
     def __init__(self, api_key, uname, pwd):
         self.headers = {'Voat-ApiKey': api_key}
         token = self.get_token(api_key, uname, pwd)
-        self.headers['Authorization'] = 'Bearer {}'.format(token)
+        if token != False:
+            self.headers['Authorization'] = 'Bearer {}'.format(token)
 
     def get_token(self, api_key, uname, pwd):
         body = 'grant_type=password&username={}&password={}'.format(uname,pwd)
         r = requests.post(TOKEN_URL, headers=self.headers, data=body)
         resp =  json.loads(r.content)
-        return resp['access_token']
+        if 'error' not in resp.keys():
+            return resp['access_token']
+        else:
+            return False
