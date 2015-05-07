99f402f2fa66d95451cd822d65237feee5bf1e37
frame11 <frame11@redchan.it>
2015-05-07 04:59:22 -0400

    Made setUpClass into class method, as it should be
    Also added some more tests of  subverse info methods.
    Now go to sleep!

--- tests.py
+++ tests.py
@@ -2,11 +2,12 @@ import getpass, unittest
 import VAPy
 
 class VAPyTests(unittest.TestCase):
-    
-    def setUpClass(self):
-        self.key = input("Please enter api_key: ")
-        self.uname = input("Please enter username: ")
-        self.pwd = getpass.getpass("Please enter password: ")
+   
+    @classmethod
+    def setUpClass(VAPyTests):
+        VAPyTests.key = input("Please enter api_key: ")
+        VAPyTests.uname = input("Please enter username: ")
+        VAPyTests.pwd = getpass.getpass("Please enter password: ")
         
         
     def setUp(self):
@@ -16,9 +17,14 @@ class VAPyTests(unittest.TestCase):
     def test_get_token(self):
         self.assertIn('Authorization', self.vapy.headers)
 
+    def test_get_subverse_creation_date(self):
+        self.assertEquals(self.vapy.get_subverse_creation_date('api'), '2015-04-08T22:46:02.477')
 
     def test_get_subverse_subscriber_count(self):
         self.assertIsInstance(self.vapy.get_subverse_subscriber_count("api"), int)
+
+    def test_get_subverse_rated_adults(self):
+        self.assertFalse(self.vapy.get_subverse_rated_adult('api'))
 
 if __name__ == '__main__':
     unittest.main()
