508e2bf1655788b034cc2d0c02eb3896292d5c9a
frame11 <frame11@redchan.it>
2015-05-26 23:40:37 -0400

    check_response() now wrapping all API call wrappers

--- VAPy/VAPy.py
+++ VAPy/VAPy.py
@@ -1,5 +1,5 @@
 import re, requests
-
+from functools import wraps
 try:
         import ujson as json
 except:
@@ -41,10 +41,17 @@ class VAPy:
     def querystring(search='', count=50):
         return '?search={}&count={}'.format(search, count)
 
+    def parse_bad_request(self, result):
+        if result['success']:
+            print("Cannot parse error for successful request. No error to parse.")
+            return None
+        else:
+            print(result)
 
     # WRAPPERS
 
     def catch_empty_input(func):
+        @wraps(func)
         def wrapper(self, *args, **kwargs):
             try:
                 result = func(self, *args, **kwargs)
@@ -54,6 +61,7 @@ class VAPy:
         return wrapper
     
     def catch_empty_filter_input(func):
+        @wraps(func)
         def wrapper(self, *args, **kwargs):
             try:
                 return(func(self, *args, **kwargs))
@@ -62,19 +70,77 @@ class VAPy:
         return wrapper
 
     def check_input_is_submission(func):
+        @wraps(func)
         def wrapper(self, inp):
             if self.is_submission(inp):
                 return func(self, inp)
             else:
                 raise ValueError('You are calling a submission-only method on something that is not a submission')
+        return wrapper
 
     def check_input_is_comment(func):
+        @wraps(func)
         def wrapper(self, inp):
             if self.is_comment(inp):
                 return func(self, inp)
             else:
                 raise ValueError('You are calling comment-only methods on something that is not a comment')
+        return wrapper
 
+    def check_response(func):
+        @wraps(func)
+        def wrapper(self, *args, **kwargs):
+            result = func(self, *args, **kwargs)
+            return result['data'] if result['success'] == True else self.parse_bad_request(result)
+        return wrapper
+
+    # VOAT CONTENT GETTERS
+
+    def get_subverse(self, subverse):
+        submissions = self.get_submissions_by_subverse(subverse)
+        return [(submission, self.get_comments_by_submission(submission)) for submission in submissions]
+
+    @check_response
+    def get_submission_by_id(self, subverse, submission_id):
+        url = API_URL + 'v/{}/{}'.format(subverse, submission_id)
+        r = requests.get(url, headers=self.headers)
+        resp = json.loads(r.content)
+        r.connection.close()
+        return resp
+
+    @check_response
+    def get_submissions_by_subverse(self, subverse):
+        url = API_URL + 'v/{}'.format(subverse)
+        r = requests.get(url, headers=self.headers)
+        resp = json.loads(r.content)
+        r.connection.close()
+        return resp
+
+    def get_content_submissions_by_subverse(self, subverse):
+        return filter(self.is_content_submission, self.get_submissions_by_subverse(subverse))
+
+    def get_url_submissions_by_subverse(self, subverse):
+        return filter(self.is_url_submission, self.get_submissions_by_subverse(subverse))
+
+    @check_response
+    def get_comment_by_id(self, comment_id):
+        url = API_URL + 'comments/{}'.format(comment_id)
+        r = requests.get(url, headers=self.headers)
+        resp = json.loads(r.content)
+        r.connection.close()
+        return resp
+    
+    @check_response
+    def get_comments_by_submission(self, submission, subverse=None):
+        if type(submission) == int:
+            url = API_URL + 'v/{}/{}/comments'.format(subverse, submission)
+        elif type(submission) == dict:
+            url = API_URL + 'v/{}/{}/comments'.format(
+                    self.get_subverse_name(submission), self.get_id(submission))
+        r = requests.get(url, headers=self.headers)
+        resp = json.loads(r.content)
+        r.connection.close()
+        return resp
 
     # SUBVERSE INFO FUNCTIONS
 
@@ -101,27 +167,37 @@ class VAPy:
     def get_subverse_sidebar(self, subverse):
         return self.subverse_info(subverse)['sidebar']
     
-    # FIND
+    # USER INFO FUNCTIONS
+
+    def user_info(self, user):
+        url = API_URL + 'u/{}/info'.format(user)
+        r = requests.get(url, headers=self.headers)
+        resp = json.loads(r.content)
+        r.connection.close()
+        return resp['data'] if resp['success'] == True else {}
 
 
     # POST
 
+    @check_response
     def post_text_submission(self, subverse, title, text):
         url = API_URL + 'v/{}'.format(subverse)
         body = json.dumps({'title':title,'content':text})
         r = requests.post(url, headers=self.headers, data=body)
         resp = json.loads(r.text)
         r.connection.close()
-        return resp['data']['id']
+        return resp
 
+    @check_response
     def post_link_submission(self, subverse, title, url):
         url = API_URL + 'v/{}'.format(subverse)
         body = json.dumps({'title':title,'url':url})
         r = requests.post(url, headers=self.headers, data=body)
         resp = json.loads(r.text)
         r.connection.close()
-        return resp['data']['id']
+        return resp
 
+    @check_response
     def post_reply_to_submission(self, submission_id, comment):
         subverse = self.get_subverse_name(self.get_submission_by_id(submission_id))
         url = API_URL + 'v/{}/{}/comment'.format(subverse, submission_id)
@@ -129,8 +205,9 @@ class VAPy:
         r = requests.post(url, headers=self.headers, data=body)
         resp = json.loads(r.text)
         r.connection.close()
-        return resp['data']['id']
+        return resp
 
+    @check_response
     def post_reply_to_comment(self, comment_id, comment):
         submission_id = self.get_comment_submission(self.get_comment_by_id(comment_id))
         subverse = self.get_subverse_name(self.get_submission_by_id(submission_id))
@@ -139,18 +216,20 @@ class VAPy:
         r = requests.post(url, headers=self.headers, data=body)
         resp = json.loads(r.text)
         r.connection.close()
-        return resp['data']['id']
+        return resp
 
+    @check_response
     def post_reply_to_pm(self, pm_id, comment):
         url = API_URL + 'comments/{}'.format(pm_id)
         body = json.dumps({'value':comment})
         r = requests.post(url, headers=self.headers, data=body)
         resp = json.loads(r.text)
         r.connection.close()
-        return resp['data']['id']
+        return resp
 
     # EDIT CONTENT
 
+    @check_response
     def edit_submission(self, submission_id, title, content):
         submission = self.get_submission_by_id(submission_id)
         subverse, s_type = self.get_subverse_name(submission), self.get_submission_type(submission)
@@ -159,19 +238,21 @@ class VAPy:
         r = requests.put(url, headers=self.headers, data=body)
         resp = json.loads(r.text)
         r.connection.close()
-        return resp['success']
+        return resp
 
+    @check_response
     def edit_comment(self, comment_id, comment):
         url = API_URL + 'comments/{}'.format(comment_id)
         body = json.dumps({'value':comment})
         r = requests.put(url, headers=self.headers, data=body)
         resp = json.loads(r.text)
         r.connection.close()
-        return resp['success']
+        return resp
 
 
     # DELETE CONTENT
 
+    @check_response
     def delete_submission(self, submission_id, title, content):
         submission = self.get_submission_by_id(submission_id)
         subverse = self.get_subverse_name(submission)
@@ -179,65 +260,21 @@ class VAPy:
         r = requests.delete(url, headers=self.headers)
         resp = json.loads(r.text)
         r.connection.close()
-        return resp['success']
+        return resp
 
+    @check_response
     def delete_comment(self, comment_id, comment):
         url = API_URL + 'comments/{}'.format(comment_id)
         r = requests.delete(url, headers=self.headers)
         resp = json.loads(r.text)
         r.connection.close()
-        return resp['success']
-
-
-    # SUBVERSE GETTER
-
-    def get_subverse(self, subverse):
-        submissions = self.get_submissions_by_subverse(subverse)
-        return [(submission, self.get_comments_to_submission(self.get_id(submission))) for submission in submissions]
+        return resp
 
 
 
-    # SUBMISSION GETTERS
+    # VOTE
 
-    def get_submission_by_id(self, submission_id):
-        url = API_URL + 'submissions/{}'.format(submission_id)
-        r = requests.get(url, headers=self.headers)
-        resp = json.loads(r.content)
-        r.connection.close()
-        return resp['data'] if resp['success'] == True else {}
-
-    def get_submissions_by_subverse(self, subverse):
-        url = API_URL + 'v/{}'.format(subverse)
-        r = requests.get(url, headers=self.headers)
-        resp = json.loads(r.content)
-        r.connection.close()
-        return resp['data'] if resp['success'] == True else []
-
-    def get_content_submissions_by_subverse(self, subverse):
-        return filter(self.is_content_submission, self.get_submissions_by_subverse(subverse))
-
-    def get_url_submissions_by_subverse(self, subverse):
-        return filter(self.is_url_submission, self.get_submissions_by_subverse(subverse))
-
-    # COMMENT GETTERS
-
-    def get_comment_by_id(self, comment_id):
-        url = API_URL + 'comments/{}'.format(comment_id)
-        r = requests.get(url, headers=self.headers)
-        resp = json.loads(r.content)
-        r.connection.close()
-        return resp['data'] if resp['success'] == True else {}
-
-    def get_comments_to_submission(self, submission):
-        if type(submission) == int:
-            subverse = self.get_subverse_name(self.get_submission_by_id(submission_id))
-        elif type(submission) == dict:
-            subverse = submission['id']
-        url = API_URL + 'v/{}/{}/comments'.format(subverse, submission_id)
-        r = requests.get(url, headers=self.headers)
-        resp = json.loads(r.content)
-        r.connection.close()
-        return resp['data'] if resp['success'] == True else []
+    #def 
 
 
     # VOAT DICT FUNCS
